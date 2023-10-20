750a1696404169f50dfd6baf6e407db8eda622fc
Paulo Vítor Reis <paulo.vrr@puccampinas.edu.br>
2023-10-20 19:43:06 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import api from './api/axios';
+import { useEffect, useState } from 'react';
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      username: username,
+      email: email,
+      password: password
+    };
+
+    try {
+      const response = await axios.post('http://localhost:8080/api/auth/signup-signup', data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response.data); // Fazer algo com os dados da resposta
+    } catch (error) {
+      console.error('Houve um erro ao enviar os dados', error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={password}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
